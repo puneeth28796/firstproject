e3025e3ecc9b7181891bb311d788ecdd3425969d
puneeth <puneethnk27@gmail.com>
2019-02-07 11:32:05 +0530

    i added the java file

---
+++ AddressInterface.java
@@ -0,0 +1,8 @@
+package com.sonata.practical.test;
+
+public interface AddressInterface {
+	public void addAddress(SingleAddress z1); 
+	public void deleteAddress(SingleAddress z1);
+	public void deleteAddress(String fname);
+	public void mailingLabel();
+}
